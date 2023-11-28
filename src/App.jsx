5a0f08804a022c5f4353af7d670a79197ac93026
Armaan Pasha <armaanpasha3@gmail.com>
2023-11-28 20:19:21 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "nes.css/css/nes.css";
+import './App.css'
+
+function App() {
+
+  function button(e) {
+    // Will continue after wednesday exam
+  }
+
+  return (
+    <>
+      <button className='nes-btn'>Hellooo</button>
+    </>
+  )
+}
+
+export default App
