c60ccb24a704c9fb16ab1195c3b59cc59343cce9
Armaan Pasha <armaanpasha3@gmail.com>
2023-12-01 20:38:33 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "nes.css/css/nes.css";
 import './App.css'
-import Loading from './loading.jsx';
+import Loading from './Loading.jsx';
 import InitButton from './InitButton.jsx';
 import Content from './Content.jsx';
 
